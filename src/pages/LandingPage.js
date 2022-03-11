fc260d04a530cbc91c33bc79f0c6efc651c254b0
Rosandymaulana <rosandy1118@gmail.com>
2022-03-11 22:12:05 +0700

    commponent-button

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import { } from "../elements/Button";
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>LandingPage</div>
+        )
+    }
+}
