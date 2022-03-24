96479ff84f92df76b976a265b98095e8276c1483
Rosandymaulana <rosandy1118@gmail.com>
2022-03-24 11:29:40 +0700

    Section Categories

--- src/parts/MostPicked.jsx
+++ src/parts/MostPicked.jsx
@@ -3,7 +3,7 @@ import Button from 'elements/Button'
 
 export default function MostPicked(props) {
     return (
-        <section className='container' ref={props.refMostPicked} style={{ height: 1000 }}>
+        <section className='container' ref={props.refMostPicked}>
             <h4 className="mb-3">Most Picked</h4>
             <div className="container-grid">
                 {props.data.map((item, index) => {
@@ -20,7 +20,7 @@ export default function MostPicked(props) {
                                     <img src={item.imageUrl} alt={item.name} className="img-cover" />
                                 </figure>
                                 <div className="meta-wrapper">
-                                    <Button type='link' className="stretched-link d-block text-white" href={`/properties/${item._id}`}>
+                                    <Button type='link' className="text-decoration-none d-block text-white" href={`/properties/${item._id}`}>
                                         <h5>{item.name}</h5>
                                     </Button>
                                     <span>
