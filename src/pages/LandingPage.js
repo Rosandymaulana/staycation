3d257e2ac052e470d6f630098bc866c9f2c825b5
Rosandymaulana <rosandy1118@gmail.com>
2022-03-25 18:21:16 +0700

    Testimony and Rating

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from 'parts/Header'
 import MostPicked from 'parts/MostPicked'
 import Categories from 'parts/Categories'
+import Testimony from 'parts/Testimony'
 
 import Hero from 'parts/Hero'
 import landingPage from 'json/landingPage.json'
@@ -18,6 +19,7 @@ export default class LandingPage extends Component {
                 <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
                 <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
                 <Categories data={landingPage.categories} />
+                <Testimony data={landingPage.testimonial} />
             </div>
         </>
     }
